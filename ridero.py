84b7787f296bbb3d744ce61a81d4a6cfe0b820a4
Artur Juraszek <asgavar@gmail.com>
2016-12-11 00:04:39 +0100

    wpisuje adres maila w pole

--- ridero.py
+++ ridero.py
@@ -20,5 +20,10 @@ voting_method_buttons = browser.find_element_by_class_name("votingButtons").find
 
 vote_by_email = voting_method_buttons[2]
 
-time.sleep(5)   # zaczekaj piec sekund (zeby okienko mialo czas by wyskoczyc)
+time.sleep(2)   # zaczekaj dwie sekundy (zeby okienko mialo czas by wyskoczyc)
 vote_by_email.click()
+
+time.sleep(2)   # j.w.
+email_field = browser.find_elements_by_class_name("p-inner")[1].find_element_by_tag_name("input")
+# TODO: zmienic zagniezdzone szukanie elementow na XPathy
+email_field.send_keys(mail_address)
