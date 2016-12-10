3c8d78c26e751df3cebc535c1285ca548572e22a
Artur Juraszek <asgavar@gmail.com>
2016-12-10 17:38:43 +0100

    dodane znajdowanie przycisku glosowania

--- ridero.py
+++ ridero.py
@@ -1,3 +1,11 @@
-import robobrowser
+from robobrowser import RoboBrowser
 
-book_url = "https://ridero.eu/pl/books/na_marginesie/"
+book_url = "https://ridero.eu/pl/books/na_marginesie/"   # ukryty, ale fizycznie przycisk juz tam jest xD
+browser = RoboBrowser()
+
+browser.open(book_url)
+
+# print(browser.parsed)
+
+vote_button = browser.get_link(u"Zagłosuj na książkę")
+print(vote_button)
