61abbeb9f17293fb08889d8e2617d8af5861148b
Artur Juraszek <asgavar@gmail.com>
2016-12-10 21:21:57 +0100

    pobieranie adresu tymczasowego maila gotowe

--- ridero.py
+++ ridero.py
@@ -1,11 +1,25 @@
+import re
 from robobrowser import RoboBrowser
 
-book_url = "https://ridero.eu/pl/books/na_marginesie/"   # ukryty, ale fizycznie przycisk juz tam jest xD
+book_url = "https://ridero.eu/pl/books/bicie_serc/"   # ukryty, ale fizycznie przycisk juz tam jest xD
+tempmail_url = "https://temp-mail.org/pl/"
 browser = RoboBrowser()
 
-browser.open(book_url)
+browser.open(tempmail_url)
 
-# print(browser.parsed)
+address_field = browser.select("#mail")   # tag <input> o id = mail
 
-vote_button = browser.get_link(u"Zagłosuj na książkę")
-print(vote_button)
+address_pattern = r"value=\".*\""   # wyrazenie regularne, szuka atrybutu value
+
+mail_address = re.findall(address_pattern, str(address_field))[0]   # zwraca liste z jednym elementem
+mail_address = mail_address[7:len(mail_address)-1]   # wyciecie value= i zostawienie samej wartosci atrybutu
+
+print(mail_address)
+
+# browser.open(book_url)
+
+# vote_button = browser.get_link(u"Zagłosuj na książkę")
+
+# inp = browser.get_forms()
+
+# print(inp)
