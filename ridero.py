00e84af02916ee7486029b6f26c13875dfdf9f3a
Artur Juraszek <asgavar@gmail.com>
2016-12-10 22:45:01 +0100

    zmiana o 180 stopni: robobrowser zmieniony na selenium + Firefox

--- ridero.py
+++ ridero.py
@@ -1,20 +1,23 @@
-import re
-from robobrowser import RoboBrowser
+# import re
+
+# from robobrowser import RoboBrowser
 
 book_url = "https://ridero.eu/pl/books/bicie_serc/"   # ukryty, ale fizycznie przycisk juz tam jest xD
 tempmail_url = "https://temp-mail.org/pl/"
-browser = RoboBrowser()
 
-browser.open(tempmail_url)
 
-address_field = browser.select("#mail")   # tag <input> o id = mail
-
-address_pattern = r"value=\".*\""   # wyrazenie regularne, szuka atrybutu value
-
-mail_address = re.findall(address_pattern, str(address_field))[0]   # zwraca liste z jednym elementem
-mail_address = mail_address[7:len(mail_address)-1]   # wyciecie value= i zostawienie samej wartosci atrybutu
-
-print(mail_address)
+# browser = RoboBrowser()
+#
+# browser.open(tempmail_url)
+#
+# address_field = browser.select("#mail")   # tag <input> o id = mail
+#
+# address_pattern = r"value=\".*\""   # wyrazenie regularne, szuka atrybutu value
+#
+# mail_address = re.findall(address_pattern, str(address_field))[0]   # zwraca liste z jednym elementem
+# mail_address = mail_address[7:len(mail_address)-1]   # wyciecie value= i zostawienie samej wartosci atrybutu
+#
+# print(mail_address)
 
 # browser.open(book_url)
 
